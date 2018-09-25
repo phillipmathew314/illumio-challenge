b72d68bdc906e82ab719b6b97a33519d1d7a3e36
Phillip Mathew <phillipmathew314@gmail.com>
2018-09-24 23:39:01 -0400

    initial commit

---
+++ firewall.py
@@ -0,0 +1,10 @@
+import csv
+
+class Firewall:
+    def __init__(self, filename):
+        self.rules = self.store_rules(filename)
+
+    def store_rules(self, filename):
+
+
+    def accept_packet(direction, protocol, port, ip_address):
