3a21d213060733df888742e9b7ab8edd116a745c
Phillip Mathew <phillipmathew314@gmail.com>
2018-09-25 01:54:35 -0400

    changed data structure

--- firewall.py
+++ firewall.py
@@ -1,4 +1,5 @@
 import csv
+import ipaddress
 
 class Firewall:
     def __init__(self, filename):
@@ -7,28 +8,28 @@ class Firewall:
     def store_rules(self, filename):
         with open(filename, 'r') as f:
             reader = csv.reader(f)
-            rules_set = set()
+            #initialize the rules dictionary
+            rules_dict = { 'inboundtcp': {'ports': set(), 'ips': set()}, 'outboundtcp': {'ports': set(), 'ips': set()}, 'inboundudp': {'ports': set(), 'ips': set()}, 'outboundudp': {'ports': set(), 'ips': set() }}
+
             for row in reader:
-                row_string = row[0] + row[1]
+                key_string = row[0] + row[1]
                 if '-' in row[2]:
-                    range = row[2].split('-')
-                    row_string += range[0]
+                    r = row[2].split('-')
+                    for i in range(int(r[0]), int(r[1])):
+                        rules_dict[key_string]['ports'].add(i)
                 else:
-                    row_string += row[2]
+                    rules_dict[key_string]['ports'].add(int(row[2]))
 
                 if '-' in row[3]:
-                    range = row[2].split('-')
-                    row_string += range[0]
+                    r = row[3].split('-')
                 else:
-                    row_string += row[3]
+                    rules_dict[key_string]['ips'].add(row[3])
 
-                rules_set.add(row_string)
-                return rules_set
+            return rules_dict
 
     def accept_packet(self, direction, protocol, port, ip_address):
-        packet_string = direction + protocol + str(port) + ip_address
-        return packet_string in self.rules
+        key_string = direction + protocol
+        return port in self.rules[key_string]['ports'] and ip_address in self.rules[key_string]['ips']
 
-f = Firewall("firewall_rules.csv")
-print(f.rules)
-print(f.accept_packet("inbound", "tcp", 80, "192.168.1.2"))
+#f = Firewall("firewall_rules.csv")
+#print(f.accept_packet("inbound", "tcp", 80, "192.168.1.2"))
